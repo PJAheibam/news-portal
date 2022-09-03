dd0406f1236786faa14bc7d5b974f173dd0a01f0
PJAheibam <prosenjitsingha28@gmail.com>
2022-09-02 23:41:00 -0700

    category displayed successful

--- js/utils.js
+++ js/utils.js
@@ -7,14 +7,31 @@ const handleCategoryClick = (event) => {
 };
 
 const getCategories = async () => {
-  const API_URL = "";
-  const res = await fetch(API_URL);
-  const data = await res.json;
-  console.log(data);
+  const API_URL = "https://openapi.programming-hero.com/api/news/categories";
+  try {
+    const res = await fetch(API_URL);
+    const {
+      data: { news_category },
+    } = await res.json();
+    displayCategories(news_category);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const displayCategories = (articles) => {
+const displayCategories = (categories) => {
   const categoryContainer = document.querySelector(".category-container");
-  articles.forEach((article) => {});
-  console.log(articles);
+  categories.forEach((category) => {
+    const btn = document.createElement("button");
+    btn.setAttribute("type", "button");
+    btn.classList.add("btn", "category");
+    btn.onclick = handleCategoryClick;
+    btn.innerText = category.category_name;
+
+    categoryContainer.appendChild(btn);
+  });
+};
+
+const fetchArticles = async (category_id) => {
+  const API_URL = `ttps://openapi.programming-hero.com/api/news/category/${category_id}`;
 };
